7a6cc6ad84f71959191711954f3968bdc97bb05f
Max Pintchouk <maxpintchouk4321@gmail.com>
2023-08-02 00:43:34 -0400

    Initial Commit

---
+++ Poker-Splitter/Poker_SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_SplitterApp.swift
+//  Poker-Splitter
+//
+//  Created by Max Pintchouk on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Poker_SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
